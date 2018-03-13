28482e5d34266d1e239d5810a87b1979b7b5b306
Diego R. Herrera <diegorherrera@gmail.com>
2018-03-13 18:02:50 +0000

    Fix production base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://api.smartbiogas.net/',
+  baseUrl: 'https://api.smartbiogas.org/',
   googleMapsApiKey: 'AIzaSyBv5hn1XUw0YSSJlML9RJpOPEMRtGwmHME'
 };

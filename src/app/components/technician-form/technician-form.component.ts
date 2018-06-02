5a7b8f9f4f4e1c5b6192dc511e845cce7a678130
Diego R. Herrera <diegorherrera@gmail.com>
2018-06-02 17:12:45 +0100

    Technician form validations WIP

--- src/app/components/technician-form/technician-form.component.ts
+++ src/app/components/technician-form/technician-form.component.ts
@@ -7,21 +7,54 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./technician-form.component.sass'],
 })
 export class TechnicianFormComponent implements OnInit {
-  technicianForm: FormGroup;
+  form: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.createForm();
   }
 
   ngOnInit() {
-    console.log(this.technicianForm);
+    console.log(this.form);
   }
 
   createForm() {
-    this.technicianForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+    this.form = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(4)]],
+      last_name: ['', Validators.required],
+      email: ['', [Validators.email, Validators.required]],
+      phone_number: [''],
+
+      country: [''],
+      // status: boolean;
+      company_name: [''],
+      contact_type: [''],
+      // resource_uri: [''],
+      // datetime_created: [''],
+      // datetime_modified: [''],
+      district: [''],
+      neighbourhood: [''],
+      other_address_details: [''],
+      postcode: [''],
+      region: [''],
+      acredit_to_install: [''],
+      acredited_to_fix: [''],
+      average_rating: [''],
+      location: [''],
+      max_num_jobs_allowed: [''],
+      number_jobs_active: [''],
+      number_of_jobs_completed: [''],
+      specialist_skills: [''],
+      // what3words: [''],
+      willing_to_travel: [''],
+      user_photo: [''],
+      village: [''],
+      ward: [''],
+      languages_spoken: [''],
     });
+  }
+
+  get firstName() {
+    return this.form.get('firstName');
   }
 
   // {“company_name”: “Alice’s biogas company”, “region”:“Arusha”, “phone_number”:“+447968684748"}
